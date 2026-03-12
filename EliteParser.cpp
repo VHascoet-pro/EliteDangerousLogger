aa404bb0aac29b8856686910ef63b7bed99bc2d8
VHascoet-pro <hascoet.vincent.pro@gmail.com>
2026-03-12 18:29:53 +0100

    Cleaning the code

--- EliteParser.cpp
+++ EliteParser.cpp
@@ -1,13 +1,15 @@
 #include "extras.h"
 #include <boost/filesystem/path.hpp>
-#include <cctype>
 
 boost::filesystem::path CustomParsers::EliteParser::JournalPathFinder(boost::filesystem::path GamePath, char OperatingSystem_selected){
   boost::filesystem::path CompleteJournalPath = GamePath;
 
-  if(std::toupper(OperatingSystem_selected) == 'W'){
+  if(std::toupper(OperatingSystem_selected) == 'W')
+  {
     CompleteJournalPath = "C:\\Users\\$USER\\Saved Games\\Frontier Developments\\Elite Dangerous";
-  } else {  
+  }
+  else
+  {  
     CompleteJournalPath = boost::filesystem::detail::dot_dot_path();
     CompleteJournalPath = boost::filesystem::detail::dot_dot_path();
     
--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
-#include "extras.h"
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/directory.hpp>
 
-#include <cstddef>
 #include <iostream>
 using std::cout, std::cin;
 using namespace boost::filesystem;
 
+#include "extras.h"
+
 path InitLog_Archive_Directory;
 
+/**
+ Main function for copying the log files
+ @param GamePath is the game path for both Windows or Linux (used only for Linux)
+ @param host to specify the OS used for the parser
+ @param LocalLogPath is the path of the Log path (./logs relatively to the main program)
+*/
 void Logger(path GamePath, char host, path LocalLogPath){
   CustomParsers::EliteParser eP;
+  
   const path GameLogPath = eP.JournalPathFinder(GamePath, host);
 }
 
@@ -28,18 +35,24 @@ int main(int argc, char* argv[1]){
     cout<<"\n\nThis is not a character proposed in the first dialogue, retry now (W/P) ? ";
     cin>>host;
   }
-  
+  /**
+   @param InitLog_Archive_Directory The path where to store the logs
+   @function Logger The function that will to the operations on the journalfiles (log files)
+  */
   if(toupper(host) == 'W'){
     InitLog_Archive_Directory = current_path().append("\\EliteLogs");
+    
     if(!exists(InitLog_Archive_Directory)){
       cout<<"The initial log archive folder does not exist, creating now...";
       create_directory(InitLog_Archive_Directory);
     }
+    
     Logger(gameLoc, host, InitLog_Archive_Directory);
   }
   else if(toupper(host) == 'P')
   {
     InitLog_Archive_Directory = current_path().append("/EliteLogs");
+    
     if(!exists(InitLog_Archive_Directory)){
       cout<<"The initial log Archive folder does not exist, creating now...";
       create_directory(InitLog_Archive_Directory);
@@ -52,6 +65,7 @@ int main(int argc, char* argv[1]){
       cout<<"This is not a valid path, please enter a correct Linux Path : ";
       cin>>gameLoc;
     }
+    
     Logger(gameLoc, host, InitLog_Archive_Directory);
   } 
 }
