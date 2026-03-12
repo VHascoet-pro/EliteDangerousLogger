0f7325bc540653dc3d6c09b059e8219e9df5798a
VHascoet-pro <hascoet.vincent.pro@gmail.com>
2026-03-12 02:25:19 +0100

    Init commit 2

--- main.cpp
+++ main.cpp
@@ -1,2 +1,36 @@
+#include <boost/filesystem/path.hpp>
 #include <iostream>
-//init code
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/directory.hpp>
+
+void WindowsLogger(){
+  const boost::filesystem::path WinLogPath = "C:\\Users\\%USER%\\Saved Games\\Frontier Developments\\Elite Dangerous";
+}
+
+void LinuxLogger(boost::filesystem::path GamePath){
+  
+}
+
+int main(int argc, char* argv[1]){
+  boost::filesystem::path gameLoc;
+  char host;
+  std::cout<<"Is your game hosted on (W)indows or (P)roton (Linux) ? ";
+  std::cin>>host;
+  while(toupper(host) == 'W' || toupper(host) == 'P'){
+    if(toupper(host) == 'W') {
+      WindowsLogger();
+    } else if(toupper(host) == 'P') {
+      std::cout<<"\nWhere is the game located ? ";
+      std::cin>>gameLoc;
+      
+      while(!boost::filesystem::exists(gameLoc)){
+        std::cout<<"This is not a valid path, please enter a correct Linux Path : ";
+        std::cin>>gameLoc;
+      }
+      LinuxLogger(gameLoc);
+    } else {
+      std::cout<<"\n\nThis is not a character proposed in the first dialogue, retry now (W/P) ? ";
+      std::cin>>host;
+    }
+  }
+}
